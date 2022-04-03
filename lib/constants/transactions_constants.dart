7a13afb185a5c8fd42fa2ffa0e57f4e32655a520
Amr Hassan <amrmhassanmarsafa@gmail.com>
2022-04-03 15:41:13 +0200

    charts utils added

---
+++ lib/constants/transactions_constants.dart
@@ -0,0 +1,100 @@
+import 'package:uuid/uuid.dart';
+
+import '../models/transaction_model.dart';
+import 'types.dart';
+
+List<TransactionModel> dummyTransactions = [
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 200,
+    createdAt: DateTime.now().subtract(Duration(days: 0)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 130,
+    createdAt: DateTime.now().subtract(Duration(days: 0)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 200,
+    createdAt: DateTime.now().subtract(Duration(days: 0)),
+    transactionType: TransactionType.outcome,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  //************************************************** */ 130
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 300,
+    createdAt: DateTime.now().subtract(Duration(days: 1)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 200,
+    createdAt: DateTime.now().subtract(Duration(days: 1)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 100,
+    createdAt: DateTime.now().subtract(Duration(days: 1)),
+    transactionType: TransactionType.outcome,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  //************************************************** */ 400
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 500,
+    createdAt: DateTime.now().subtract(Duration(days: 2)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 200,
+    createdAt: DateTime.now().subtract(Duration(days: 2)),
+    transactionType: TransactionType.income,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  TransactionModel(
+    id: Uuid().v4(),
+    title: 'title',
+    description: 'description',
+    amount: 200,
+    createdAt: DateTime.now().subtract(Duration(days: 2)),
+    transactionType: TransactionType.outcome,
+    ratioToTotal: 0,
+    profileId: '0',
+  ),
+  //************************************************** */ 500
+];
